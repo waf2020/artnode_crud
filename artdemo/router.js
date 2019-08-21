47c3690970df96cd9f29a0756fa479ee0647e35b
ruby <549426210@qq.com>
2019-08-21 17:57:00 +0800

    设计路由

--- artdemo/router.js
+++ artdemo/router.js
@@ -1,6 +1,23 @@
 
+const express=require('express');
+//创建路由容器
+const router=express.Router();
 
-module.exports=(app)=>{
+
+
+
+//将路由挂在到router路由容器内
+router.get("/student",(req,res)=>{
+
+});
+
+
+
+
+
+
+
+/*module.exports=(app)=>{
 	console.log(app);
 	app.get("/",(req,res)=>{
    fs.readFile("./db.json","utf8",(err,data)=>{
@@ -21,4 +38,4 @@ module.exports=(app)=>{
    
 	
 });
-}
+}*/
